e2ff474ed2ca6ce172aa57a45b0a1e71daa1a363
Gerald Haesendonck <gerald.haesendonck@ugent.be>
2026-01-30 17:17:46 +0100

    Added shape matching

--- src/main/java/be/ugent/idlab/knows/wc2/Main.java
+++ src/main/java/be/ugent/idlab/knows/wc2/Main.java
@@ -8,52 +8,17 @@ import java.nio.file.Path;
 public class Main {
 
     static void main() throws IOException {
-        Path scenarioDir = Path.of("/home/geraldh/projects/2024_bocemon/code/use-case-scenario-c-thermal-fno-steps/scenario_manually/");
-        //Path scenarioDir = Path.of("/home/geraldh/projects/oslo/oslo-steps-workflow-composer/scenarios/stepsOR");
+        //Path scenarioDir = Path.of("/home/geraldh/projects/2024_bocemon/code/use-case-scenario-c-thermal-fno-steps/scenario_manually/");
+        Path scenarioDir = Path.of("/home/geraldh/projects/oslo/oslo-steps-workflow-composer/scenarios/stepsAND");
         Path shapesPath = scenarioDir.resolve("shapes.ttl");
         Path statesPath = scenarioDir.resolve("states.ttl");
         Path stepsPath = scenarioDir.resolve("steps.ttl");
         Path goalStatesPath = scenarioDir.resolve("goalStates.txt");
 
+        Path contextPath = scenarioDir.resolve("data_04.ttl");
+
         QueryGraphBuilder queryGraphBuilder = QueryGraphBuilder.create(stepsPath.toString(), goalStatesPath.toString(), shapesPath.toString(), statesPath.toString());
         QueryGraph queryGraph = queryGraphBuilder.build();
-
-
-//        Map<Node, Node> propertyShapeToState = getPropertyShapeToStateMap(shapesGraph, statesGraph);
-//
-//
-//        Graph dataGraph = RDFDataMgr.loadGraph("/home/geraldh/projects/2024_bocemon/code/use-case-scenario-c-thermal-fno-steps/scenario_manually/data_03_people_requests.ttl");
-//        // TODO: enrich data graph with results from knowledge.n3
-//
-//        Shapes shapes = Shapes.parse(shapesGraph);
-//
-//        ValidationReport report = ShaclValidator.get().validate(shapes, dataGraph);
-//        for (ReportEntry entry : report.getEntries()) {
-//            Node focusNode = entry.focusNode(); // room1
-//            Node sourceNode = entry.source();   // the shacl property shape
-//            System.out.println("focusNode = " + focusNode.getURI() + ";  sourceNode = " + sourceNode.getURI());
-//        }
-        //ShLib.printReport(report);
-
+        queryGraph.process(contextPath.toString());
     }
-
-
-//    private static Map<Node, Node> getPropertyShapeToStateMap(final Graph shapesGraph, final Graph statesGraph) {
-//        Map<Node, Node> propertyShapeToState = new HashMap<>();
-//        // Find which StateShape has which PropertyShape
-//        for (ExtendedIterator<Triple> shapeIt = shapesGraph.find(Node.ANY, shaclPropertyPredicate, Node.ANY); shapeIt.hasNext(); ) {
-//            Triple shapeTriple = shapeIt.next();
-//            Node propertyShapeNode = shapeTriple.getObject();
-//            Node stateShapeNode = shapeTriple.getSubject();
-//
-//            // Find which State has which StateShape, and add to map ProperyShape -> State
-//            for (ExtendedIterator<Triple> stateIt = statesGraph.find(Node.ANY, stateShapePredicate, stateShapeNode); stateIt.hasNext(); ) {
-//                Triple stateTriple = stateIt.next();    // *should* be one
-//                Node stateNode = stateTriple.getSubject();
-//                propertyShapeToState.put(propertyShapeNode, stateNode);
-//            }
-//        }
-//        return propertyShapeToState;
-//    }
-//
 }
--- src/main/java/be/ugent/idlab/knows/wc2/graph/QueryGraph.java
+++ src/main/java/be/ugent/idlab/knows/wc2/graph/QueryGraph.java
@@ -1,17 +1,53 @@
 package be.ugent.idlab.knows.wc2.graph;
 
 import org.apache.jena.graph.Graph;
+import org.apache.jena.graph.Node;
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.shacl.ShaclValidator;
+import org.apache.jena.shacl.Shapes;
+import org.apache.jena.shacl.ValidationReport;
+import org.apache.jena.shacl.validation.ReportEntry;
 
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class QueryGraph {
     private final Map<String, State> states;
     private final Graph shapesGraph;
     private final Map<String, String> shapeToState;
 
-    public QueryGraph(final Map<String, State> states, final Graph shapesGraph, Map<String, String> shapeToState) {
+    private final Shapes shapes;
+
+    public QueryGraph(final Map<String, State> states,
+                      final Graph shapesGraph,
+                      final Map<String, String> shapeToState) {
         this.states = states;
         this.shapesGraph = shapesGraph;
         this.shapeToState = shapeToState;
+        shapes = Shapes.parse(shapesGraph);
+    }
+
+    /**
+     * Calculate matching states, next steps to take, ...
+     * @param contextFile   The context data + reasoned knowledge
+     */
+    public void process(final String contextFile) {
+        Graph context = RDFDataMgr.loadGraph(contextFile);
+
+        // Which shapes/states DO NOT match the context (invalid)?
+        Set<String> nonMatchingShapes = new HashSet<>();
+        ValidationReport report = ShaclValidator.get().validate(shapes, context);
+        for (ReportEntry entry : report.getEntries()) {
+            Node condextNode = entry.focusNode(); // The matching context node
+            Node shapeNode = entry.source();
+            System.out.println(condextNode.getURI() + " does NOT match " + shapeNode.getURI());
+            nonMatchingShapes.add(shapeNode.getURI());
+        }
+
+        Set<String> matchingShapes = new HashSet<>(shapeToState.keySet());
+        matchingShapes.removeAll(nonMatchingShapes);
+
+        System.out.println("Matching shapes: " + matchingShapes);
     }
 }
