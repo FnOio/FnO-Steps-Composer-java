69367cdd284039baee09eeb1eadaf93cab362109
Gerald Haesendonck <gerald.haesendonck@ugent.be>
2026-01-30 11:42:24 +0100

    Working recursive to build graph in one go.

--- src/main/java/be/ugent/idlab/knows/wc2/QueryGraphBuilder.java
+++ src/main/java/be/ugent/idlab/knows/wc2/QueryGraphBuilder.java
@@ -1,6 +1,8 @@
 package be.ugent.idlab.knows.wc2;
 
+import be.ugent.idlab.knows.wc2.graph.Operator;
 import be.ugent.idlab.knows.wc2.graph.State;
+import be.ugent.idlab.knows.wc2.graph.State2;
 import be.ugent.idlab.knows.wc2.graph.Step;
 import org.apache.jena.graph.Graph;
 import org.apache.jena.graph.Node;
@@ -15,6 +17,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
 
+import static be.ugent.idlab.knows.wc2.graph.Operator.*;
+
+
 public class QueryGraphBuilder {
     private final Graph shapesGraph;
     private final Graph stepsGraph;
@@ -22,6 +27,7 @@ public class QueryGraphBuilder {
     private final Set<String> goalStates;
 
     private final Map<String, State> processedStates = new HashMap<>();
+    private final Map<String, State2> processedStates2 = new HashMap<>();
     private final Map<String, Step> processedSteps = new HashMap<>();
 
     private static final Node shaclPropertyPredicate = NodeFactory.createURI("http://www.w3.org/ns/shacl#property");
@@ -76,17 +82,91 @@ public class QueryGraphBuilder {
         // first build graph
         for (String goalStateIri : goalStates) {
             Node goalState = NodeFactory.createURI(goalStateIri);
-            processPathsBackwards(goalState, true, 0);
+            //processPathsBackwards(goalState, true, 0);
+            processPathsBackwards2(goalState, new Stack<>(), null, null);
         }
 
-        // then apply the right operators
+/*        // then apply the right operators
         for (String goalStateIri : goalStates) {
             State goalState = processedStates.get(goalStateIri);
             goalState.fixOperator();
             goalState.pushBackGoalState();
-        }
+        }*/
 
         System.out.println("Done!");
+    }
+
+    private void processPathsBackwards2(final Node currentStateNode,
+                                        final Stack<Operator> operators,
+                                        final String nextStepIRI,
+                                        final State2 nextState
+    ) {
+        int indentation = operators.size() * 2;
+        String currentStateIRI = currentStateNode.getURI();
+        System.out.println(("* currentState = " + currentStateIRI).indent(indentation));
+        System.out.println(("  operators: " + operators).indent(indentation));
+
+        boolean currentStateAlreadyVisited = false;
+
+        State2 currentState;
+        if (processedStates2.containsKey(currentStateIRI)) {
+            currentState = processedStates2.get(currentStateIRI);
+            currentStateAlreadyVisited = true;
+        } else {
+            currentState = new State2(currentStateIRI);
+            processedStates2.put(currentStateIRI, currentState);
+        }
+
+        if (nextStepIRI != null && nextState != null) {
+            currentState.addNextState(nextStepIRI, nextState);
+        }
+
+        if (currentStateAlreadyVisited) {
+            return;
+        }
+
+        // check if there are multiple outgoing steps
+        List<Triple> outgoingSteps = stepsGraph.find(Node.ANY, requiresStatePredicate, currentStateNode).toList();
+        if (outgoingSteps.size() > 1) {
+            Operator operator = operators.empty() ? XOR : operators.pop();
+            currentState.setOperator(operator);
+            System.out.println(("Split! Operator: " + operator).indent(indentation));
+            // subPath...
+        }
+
+        // The recursion ends when we reached the start state, emptyState!
+        if (currentStateNode.equals(emptyState)) {
+            // done!
+            return;
+        }
+
+        List<Triple> incomingSteps = stepsGraph.find(Node.ANY, producesStatePredicate, currentStateNode).toList();
+
+        // Check if there are multiple incoming steps; this means the operator is XOR
+        // where the original path split.
+        if (incomingSteps.size() > 1) {
+            operators.push(XOR);
+            currentState.setEndOfOperator(XOR);
+        } else {
+            // AND when incoming step has more start (requires) states
+            Node incomingStep = incomingSteps.getFirst().getSubject();
+            int startStatesForStep = stepsGraph.find(incomingStep, requiresStatePredicate, Node.ANY).toList().size();
+            if (startStatesForStep > 1) {
+                operators.push(AND);
+                currentState.setEndOfOperator(AND);
+            }
+        }
+
+        // Go to next recursive iteration by going the path(s) backward!
+        for (Triple incomingStep : incomingSteps) {
+            Node previousStep = incomingStep.getSubject();
+            String previousStepIRI = previousStep.getURI();
+            System.out.println(("previousStep = " + previousStepIRI).indent(indentation));
+            for (ExtendedIterator<Triple> previousStateIter = stepsGraph.find(previousStep, requiresStatePredicate, Node.ANY); previousStateIter.hasNext(); ) {
+                Triple previousStateTriple = previousStateIter.next();
+                processPathsBackwards2(previousStateTriple.getObject(), operators, previousStepIRI, currentState);
+            }
+        }
     }
 
     private void processPathsBackwards(final Node currentStateNode, boolean isGoalState, final int stackDepth) {
